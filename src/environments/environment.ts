88ccc7905058516d62e2f17a0c02a099f41b932f
carlos_daniel <carloschoqque123@gmail.com>
2024-03-31 13:33:25 -0500

    deleted environments

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  authUrl: 'http://localhost:8000/api/v1/auth/login',
-  registerUrl: 'http://localhost:8000/api/v1/auth/register',
-  logoutUrl: 'http://localhost:8000/api/v1/auth/logout',
-  usersUrl: 'http://localhost:8000/api/v1/user',
-  deleteRoomUrl: 'http://localhost:8000/api/v1/rooms',
-}
