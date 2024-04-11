1a91e106630acdf9c68940e53186a26ba88c742e
carlos_daniel <carloschoqque123@gmail.com>
2024-04-11 16:26:46 -0500

    Display persistent recordings component

--- src/app/modules/main/recordings/recordings.component.ts
+++ src/app/modules/main/recordings/recordings.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
-import { TemporalRecordings } from './components/temporal-recordings/temporal-recordings.component'
+import { TemporalRecordingsComponent } from './components/temporal-recordings/temporal-recordings.component'
+import { PersistentRecordingsComponent } from './components/persistent-recordings/persistent-recordings.component'
 
 @Component({
   selector: 'main-recordings',
   templateUrl: 'recordings.component.html',
   standalone: true,
-  imports: [TemporalRecordings]
+  imports: [TemporalRecordingsComponent, PersistentRecordingsComponent]
 })
 export class RecordingsComponent {
 
