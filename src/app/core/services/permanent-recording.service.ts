5d36f7b83056aef28c2029a7e3d59d0f70546e2e
carlos_daniel <carloschoqque123@gmail.com>
2024-04-11 17:15:14 -0500

    Refactor permanen-recording service and add new method in s3-audio service

--- src/app/core/services/permanent-recording.service.ts
+++ src/app/core/services/permanent-recording.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, inject } from "@angular/core";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import {environment} from "@environments/environment"
 import { TokenService } from "./token.service";
 import { DataService } from "./data.service";
+import { S3AudioService } from "./s3-audio.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,37 +9,21 @@ import { DataService } from "./data.service";
 export class PermanentRecordingService{
   private tokenService = inject(TokenService)
   private dataService = inject(DataService)
-  private client: S3Client
-
-  constructor(){
-    this.client = new S3Client({
-      credentials: {
-        accessKeyId: environment.CLIENT_ACCESS_KEY,
-        secretAccessKey: environment.CLIENT_SECRET_KEY
-      },
-      region: environment.BUCKET_REGION
-    })
-  }
+  private s3AudioService = inject(S3AudioService)
 
   saveRecording = async (url: URL) => {
     const cache = await caches.open("audios")
     const r = await cache.match(url)
-    const blob = await r?.blob()
+    const blob = await r?.blob()!
     const prefix = this.tokenService.getUserId()
     const recordId = url.pathname.split('/').pop()!
 
     const key = `${prefix}/${recordId}`
 
-    const putCommand = new PutObjectCommand({
-      Bucket: environment.BUCKET_NAME,
-      Key: key,
-      CacheControl: "max-age="+60*60*24,
-      Body: blob
-    })
     const data = JSON.parse(localStorage.getItem(recordId)!)
 
     await Promise.all([
-      this.client.send(putCommand),
+      this.s3AudioService.saveImage(blob, key),
       this.dataService.addRecording({
         id: recordId,
         name: data.name,
--- src/app/core/services/s3-audio.service.ts
+++ src/app/core/services/s3-audio.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from "@angular/core";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { environment } from "@environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class S3AudioService{
+  private client: S3Client
 
+  constructor(){
+    this.client = new S3Client({
+      credentials: {
+        accessKeyId: environment.CLIENT_ACCESS_KEY,
+        secretAccessKey: environment.CLIENT_SECRET_KEY
+      },
+      region: environment.BUCKET_REGION
+    })
+  }
+
+  saveImage = async (blob: Blob, key: string) => {
+    const putCommand = new PutObjectCommand({
+      Bucket: environment.BUCKET_NAME,
+      Key: key,
+      CacheControl: "max-age="+60*60*24,
+      Body: blob
+    })
+    await this.client.send(putCommand)
+  }
 }
