eb122d80ecbaffb4b50fd76f27574a200bc27a8c
carlos_daniel <carloschoqque123@gmail.com>
2024-04-10 22:28:54 -0500

    Implement the saveRecording method This method is responsible of getting the audio from cache and then save it to aws S3 service. This method also saves in the api  aditional data that describes the recording data

--- src/app/core/services/permanent-recording.service.ts
+++ src/app/core/services/permanent-recording.service.ts
@@ -2,12 +2,14 @@ import { Injectable, inject } from "@angular/core";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import {environment} from "@environments/environment"
 import { TokenService } from "./token.service";
+import { DataService } from "./data.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermanentRecordingService{
   private tokenService = inject(TokenService)
+  private dataService = inject(DataService)
   private client: S3Client
 
   constructor(){
@@ -25,12 +27,26 @@ export class PermanentRecordingService{
     const r = await cache.match(url)
     const blob = await r?.blob()
     const prefix = this.tokenService.getUserId()
+    const recordId = url.pathname.split('/').pop()!
+
+    const key = `${prefix}/${recordId}`
+
     const putCommand = new PutObjectCommand({
       Bucket: environment.BUCKET_NAME,
-      Key: "",
+      Key: key,
       CacheControl: "max-age="+60*60*24,
-      Body: "Buffer"
+      Body: blob
     })
-    await this.client.send(putCommand)
+    const data = JSON.parse(localStorage.getItem(recordId)!)
+
+    await Promise.all([
+      this.client.send(putCommand),
+      this.dataService.addRecording({
+        id: recordId,
+        name: data.name,
+        creationDate: data.date,
+        recordingKey: key
+      })
+    ])
   }
 }
