0d4718fa2e2a6667f9d1ef22ada268528bb35854
carlos_daniel <carloschoqque123@gmail.com>
2024-04-12 11:39:52 -0500

    Update home page Now display information about the project

--- src/app/modules/main/home/home.component.ts
+++ src/app/modules/main/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { User } from "@models/data.model";
+import { DataService } from "@services/data.service";
+import { Subscription } from "rxjs";
 
 @Component({
   templateUrl: './home.component.html',
@@ -6,4 +9,16 @@ import { Component } from "@angular/core";
   standalone: true
 })
 export class HomeComponent{
+  userSubscription: Subscription
+  private dataService = inject(DataService)
+  user: User
+
+  ngOnInit(){
+    this.userSubscription = this.dataService.getUser().subscribe(_user => {
+      this.user = _user
+    })
+  }
+  ngOnDelete(){
+    this.userSubscription.unsubscribe()
+  }
 }
