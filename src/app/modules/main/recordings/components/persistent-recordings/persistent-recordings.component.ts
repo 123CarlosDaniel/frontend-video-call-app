11b183259d7b23159795489a74f21d5a72c71460
carlos_daniel <carloschoqque123@gmail.com>
2024-04-11 21:06:55 -0500

    Display persistent recordings data

--- src/app/modules/main/recordings/components/persistent-recordings/persistent-recordings.component.ts
+++ src/app/modules/main/recordings/components/persistent-recordings/persistent-recordings.component.ts
@@ -1,23 +1,30 @@
+import { DatePipe } from "@angular/common";
 import { Component, inject } from "@angular/core";
-import { RecordingData } from "@models/data.model";
+import { RecordingDataExtra } from "@models/data.model";
 import { DataService } from "@services/data.service";
+import { S3AudioService } from "@services/s3-audio.service";
 import { Subscription } from "rxjs";
 
 @Component({
   templateUrl: "./persistent-recordings.component.html",
   standalone: true,
-  selector: "persistent-recordings"
+  selector: "persistent-recordings",
+  imports: [DatePipe]
 })
 export class PersistentRecordingsComponent{
   private dataService = inject(DataService)
-  recordingsData: RecordingData[]
-
+  private s3AudioService = inject(S3AudioService)
+  recordingsData: RecordingDataExtra[]
   userSubscription: Subscription
 
   ngOnInit(){
     this.userSubscription = this.dataService.getUser().subscribe(async _user => {
-      this.recordingsData = _user.recordings
-      console.log(this.recordingsData)
+      const promises = _user.recordings.map<Promise<RecordingDataExtra>>(async r => {
+        const url = await this.s3AudioService.getImageUrl(r.recordingKey)
+        return {...r, objectUrl: url}
+      })
+      if(promises.length == 0) return
+      this.recordingsData = await Promise.all(promises)
     })
   }
 
